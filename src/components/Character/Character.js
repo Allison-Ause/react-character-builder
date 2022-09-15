56f7dcb627da569a0240e051476448404d7aaad8
Allison Ause <allison.ause@gmail.com>
2022-09-15 15:16:18 -0700

    styled Character.js

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -5,9 +5,9 @@ export default function Character({ head, body, bottoms }) {
     <div className="character">
       <h1>You At Your Coziest:</h1>
       <div className="images">
-        <img src={`${process.env.PUBLIC_URL}/${head}.png`} />
-        <img src={`${process.env.PUBLIC_URL}/${body}.png`} />
-        <img src={`${process.env.PUBLIC_URL}/${bottoms}.png`} />
+        <img className="head" src={`${process.env.PUBLIC_URL}/${head}.png`} />
+        <img className="body" src={`${process.env.PUBLIC_URL}/${body}.png`} />
+        <img className="bottoms" src={`${process.env.PUBLIC_URL}/${bottoms}.png`} />
       </div>
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,7 +6,7 @@ import Controls from '../Controls/Controls';
 import Catchphrase from '../Catchphrase/Catchphrase';
 import Display from '../Display/Display';
 import Character from '../Character/Character';
-import background from '../../backgrounds/armchair.jpg';
+import background from '../../backgrounds/blanket.jpg';
 
 export default function Main() {
   const [head, setHead] = useState('pompom-head');
@@ -18,30 +18,31 @@ export default function Main() {
   const [catchphrases, setCatchphrases] = useState([]);
   const [currentCatchphrase, setCurrentCatchphrase] = useState('');
   return (
-    <div className="main" style={{ backgroundImage: `url(/backgrounds/${background})` }}>
-      {/* TODO: come back to background after shrinking components */}
-      <Controls
-        head={head}
-        setHead={setHead}
-        setHeadCount={setHeadCount}
-        body={body}
-        setBody={setBody}
-        setBodyCount={setBodyCount}
-        bottoms={bottoms}
-        setBottoms={setBottoms}
-        setBottomsCount={setBottomsCount}
-      />
-      <Catchphrase
-        setCatchphrases={setCatchphrases}
-        currentCatchphrase={currentCatchphrase}
-        setCurrentCatchphrase={setCurrentCatchphrase}
-      />
-      <Display
-        headCount={headCount}
-        bodyCount={bodyCount}
-        bottomsCount={bottomsCount}
-        catchphrases={catchphrases}
-      />
+    <div className="main" style={{ backgroundImage: `url(${background})` }}>
+      <div className="tools">
+        <Controls
+          head={head}
+          setHead={setHead}
+          setHeadCount={setHeadCount}
+          body={body}
+          setBody={setBody}
+          setBodyCount={setBodyCount}
+          bottoms={bottoms}
+          setBottoms={setBottoms}
+          setBottomsCount={setBottomsCount}
+        />
+        <Catchphrase
+          setCatchphrases={setCatchphrases}
+          currentCatchphrase={currentCatchphrase}
+          setCurrentCatchphrase={setCurrentCatchphrase}
+        />
+        <Display
+          headCount={headCount}
+          bodyCount={bodyCount}
+          bottomsCount={bottomsCount}
+          catchphrases={catchphrases}
+        />
+      </div>
       <Character head={head} body={body} bottoms={bottoms} />
     </div>
   );
